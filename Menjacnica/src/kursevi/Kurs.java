8d9af244b539079567fd2cedd85417e9039d0c3a
aleksandradragojevic1995 <sana.dragojevic@gmail.com>
2016-03-27 13:36:59 +0200

    Ubacene su metode toString, hashCode i equals u klase Kurs i Valuta.

--- Menjacnica/src/kursevi/Kurs.java
+++ Menjacnica/src/kursevi/Kurs.java
@@ -39,4 +39,32 @@ public class Kurs {
 		this.datum = datum;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajni);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(srednji);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Kurs) {
+			Kurs k = (Kurs) obj;
+			return k.getKupovni() == kupovni && k.getProdajni() == prodajni && k.getSrednji() == srednji;
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Kurs [kupovni: " + kupovni + ", prodajni: " + prodajni + ", srednji: " + srednji + ", datum: " + datum.getTime() + "]";
+	}
 }
--- Menjacnica/src/valute/Valuta.java
+++ Menjacnica/src/valute/Valuta.java
@@ -34,4 +34,28 @@ public class Valuta {
 		this.kursevi = kursevi;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kursevi == null) ? 0 : kursevi.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Valuta) {
+			Valuta v = (Valuta) obj;
+			return v.getNaziv().equals(naziv);
+		}
+		return false;
+	}
+
+	@Override
+	public String toString() {
+		return "Valuta [naziv: " + naziv + ", skraceniNaziv: " + skraceniNaziv + ", kursevi: " + kursevi + "]";
+	}
+	
 }
